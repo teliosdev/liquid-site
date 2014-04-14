7a2a9179d27315fe406b765df4a2255cd74740fb
Jeremy Rodi <redjazz96@gmail.com>
2014-04-14 17:14:35 -0400

    Fix 500 error

--- run.rb
+++ run.rb
@@ -38,13 +38,22 @@ end
 post '/exec.js' do
   ast    = params[:type] == "ast"
   tokens = params[:type] == "tokens"
+  body   = params[:code]
+
+  if body.nil? or body.empty?
+    status 400
+    return "No data to compile"
+  end
 
   begin
-    output = Liquidscript.compile(params[:code],
+    output = Liquidscript.compile(body,
       :ast => ast, :tokens => tokens).to_s
   rescue Liquidscript::Error => e
     status 400
     return e.message
+  rescue => e
+    status 500
+    return "An error occured proccessing your request."
   end
 
   content_type 'application/javascript'
